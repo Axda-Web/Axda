c33b989d92ad93434dbc71dd8294950c30c5aebc
Alyx DARENNE <axda.web@gmail.com>
2023-08-10 17:26:02 +0200

    Update API service functions

--- services/educations.ts
+++ services/educations.ts
@@ -1,9 +1,9 @@
 import { EducationsSchema } from "../models/zod-schemas";
 
-const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "http://localhost:3000";
 
 const getAll = async () => {
-  const res = await fetch(`${BASE_URL}/api/educations`);
+  const res = await fetch(`/api/educations`);
   const data = await res.json();
   return EducationsSchema.parse(data);
 };
