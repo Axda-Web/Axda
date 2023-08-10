02c4474f6de3edd63c03365c5c31d45571a85d55
Alyx DARENNE <axda.web@gmail.com>
2023-08-10 17:52:25 +0200

    Update route API URLs

--- services/experiences.ts
+++ services/experiences.ts
@@ -1,9 +1,9 @@
 import { ExperiencesSchema } from "../models/zod-schemas";
 
-const BASE_URL = `https://${process.env.VERCEL_URL}`;
+// const BASE_URL = `https://${process.env.VERCEL_URL}`;
 
 const getAll = async () => {
-  const res = await fetch(`${BASE_URL}/api/experiences`);
+  const res = await fetch(`${process.env.VERCEL_URL}/api/experiences`);
   const data = await res.json();
   return ExperiencesSchema.parse(data);
 };
