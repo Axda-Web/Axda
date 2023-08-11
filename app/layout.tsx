d5c4e2453a76d7dfb39df44b842e442e37882e4a
Alyx DARENNE <axda.web@gmail.com>
2023-08-11 11:10:47 +0200

    Add google analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import Footer from "./components/footer";
 import AnimationWrapper from "./components/animation-wrapper";
 
 const inter = Inter({ subsets: ["latin"] });
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export const metadata: Metadata = {
   title: "latest Next version",
@@ -21,7 +22,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body className="bg-light dark:bg-dark">
+        {/* eslint-disable-next-line */}
         <Script id="theme-switcher" strategy="beforeInteractive">
           {`if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark')
@@ -34,6 +45,11 @@ export default function RootLayout({
           <AnimationWrapper>{children}</AnimationWrapper>
           <Footer />
         </div>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   );
