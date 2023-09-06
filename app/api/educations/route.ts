52e7c0d299c3b9940470322a0384f6974ed42135
Alyx DARENNE <axda.web@gmail.com>
2023-09-06 21:37:37 +0200

    Update content: Remove useless files and svgs

--- app/api/educations/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export async function GET() {
-  const educations = await prisma.education.findMany();
-  return NextResponse.json(educations);
-}
